548d41652c37c50612fdbbef957e733c60f7a165
Denny Israel <denny.israel@ottogroup.com>
2019-12-13 09:33:54 +0100

    cleanup script extracted

--- certs/new.sh
+++ certs/new.sh
@@ -1,14 +1,4 @@
-echo
-echo "======="
-echo "cleanup"
-echo "======="
-echo
-rm ../backend/src/main/resources/keystore/certAndKey.p12
-rm ../client/truststore.p12
-rm -r backend
-rm -r root
-mkdir backend
-mkdir root
+./cleanup.sh
 
 # see: https://superuser.com/questions/126121/how-to-create-my-own-certificate-chain
 echo
