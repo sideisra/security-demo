9b9c80824f714a13c345fa02cb9b935410d1e24c
Denny Israel <denny.israel@ottogroup.com>
2020-03-06 14:37:44 +0100

    backend working in docker container

--- backend/src/main/docker/docker-entrypoint.sh
+++ backend/src/main/docker/docker-entrypoint.sh
@@ -6,6 +6,7 @@ java \
     -Djava.security.policy==/app/AllPermissions.policy \
     -Djava.security.debug=access \
     -Djdk.io.permissionsUseCanonicalPath=true \
+    -Dsun.misc.URLClassPath.disableJarChecking=true \
     -jar /app/backend.jar \
     --spring.profiles.active=docker \
     2> out.txt
